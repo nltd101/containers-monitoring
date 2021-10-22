b280f9cad6760bcd80eed7dd43f87549dd6365a9
nltd101 <nltd101@gmail.com>
2021-10-22 21:22:11 +0700

    add be

--- backend/apps/serializers.py
+++ backend/apps/serializers.py
@@ -1,5 +1,15 @@
 from django.contrib.auth.models import User, Group
+from django.contrib.postgres import fields
+from django.db import models
 from rest_framework import serializers
+
+from apps.models import OrderModel
+
+
+class ContainerModelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = OrderModel
+        fields = ['name', 'fee']
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
