1b8dbeb802b4bfe474cbbfd983aaea9e1d2b80b6
nltd101 <nltd101@gmail.com>
2021-11-01 21:42:34 +0700

    add api order

--- backend/apps/models.py
+++ backend/apps/models.py
@@ -1,4 +1,6 @@
-from rest_framework.exceptions import ValidationError
+from typing import List
+from django.db.models.query import QuerySet
+from rest_framework.exceptions import APIException, ValidationError
 from django.forms.models import model_to_dict
 from django.db import models
 from django.db.models.expressions import F
@@ -11,7 +13,32 @@ from apps.utils import json_to_array_factor
 
 class ContainerModel(models.Model):
     name = models.CharField(max_length=20)
-    order_id = models.IntegerField(null=True)
+    order_id = models.IntegerField(null=True,blank=True)
+
+    @classmethod
+    def get_sorted_container_list(cls):
+        containers:QuerySet[ContainerModel] = cls.objects.all().order_by("order_id")
+        busy_container =[]
+        free_container=[]
+        for container_object in containers:
+            order:OrderModel = OrderModel.get_by_id(container_object.order_id)
+            container_data=model_to_dict(container_object)
+            if (order):
+                print(order)
+                container_data.update({"start_time":order.start_time})
+                container_data.update({"last_update":order.last_update})
+                container_data.update({"free":False})
+                busy_container.append(container_data)
+           
+            else:
+                container_data.update({"start_time":None})
+                container_data.update({"last_update":None})
+                container_data.update({"free":True})
+                free_container.append(container_data)
+        busy_container.sort(key=lambda x:x["start_time"])
+        busy_container.extend(free_container)
+        return busy_container
+
 
     def __str__(self) -> str:
         return str(self.pk)+"_"+str(self.name)
@@ -26,6 +53,16 @@ class OrderModel(models.Model):
     category = models.IntegerField(default=0)
     container = models.ForeignKey(ContainerModel, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return str(self.pk)
+
+    @classmethod
+    def get_by_id(cls,id):
+        result = cls.objects.filter(pk=id)
+        if not result:
+            raise ValidationError(detail="no such an order")
+        return model_to_dict(result.first())
+
     @classmethod
     def create(cls, data: dict):
         name = data.get("name")
@@ -38,6 +75,12 @@ class OrderModel(models.Model):
                                   start_time=start_time, category=category, container=container)
         return model_to_dict(data)
 
+    @classmethod
+    def get_all_order(cls,page=0,step=10):
+        data_object = cls.objects.all().order_by("-start_time")[page*step:(page+1)*step]
+        print(data_object)
+       
+        return [model_to_dict(object) for object in data_object]
 
 class PackageModel(models.Model):
     order = models.ForeignKey(OrderModel, on_delete=models.CASCADE)
@@ -63,24 +106,27 @@ class PackageModel(models.Model):
     def create(cls, container_id, data: dict):
         container: ContainerModel = ContainerModel.objects.get(pk=container_id)
         if not container:
-            raise ValidationError
+            raise ValidationError(detail="no such a container")
         order_id = container.order_id
         data_time = data.get("data_time")
         json_data = data.get("json_data")
         order = OrderModel.objects.get(pk=order_id)
-        # if not OR:
-        #     raise ValidationError
+      
         data = json_to_array_factor(json_data)
         if not data:
-            raise ValidationError
+            raise ValidationError(detail="the data format is invalid")
         object = cls.objects.create(
             order=order, data_time=data_time, data=data)
         return model_to_dict(object)
 
+ 
     @ classmethod
     def get_package_by_order_id(cls, order_id):
-        object_array = cls.objects.filter(order=OrderModel.objects.get(
-            pk=order_id)).order_by("data_time")
+        orders = OrderModel.objects.filter(pk=order_id)
+        if not orders:
+            raise ValidationError(detail="no suck an order")
+        object_array = cls.objects.filter(order=orders.first()).order_by("data_time")
+      
         return [model_to_dict(object) for object in object_array]
 
     def __str__(self) -> str:
--- backend/apps/views.py
+++ backend/apps/views.py
@@ -5,41 +5,34 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound
-from apps.models import OrderModel, PackageModel
+from apps.models import ContainerModel, OrderModel, PackageModel
 from rest_framework.request import Request
 
 
 class OrderView(APIView):
-    def patch(self,  request: Request):
-
-        data = OrderModel.create(request.data)
-        print(data)
-        return Response({"data": data})
-
+    def get(self,request:Request):
+        order_id = request.query_params.get("order_id")
+        if (order_id):
+            return Response({"data":PackageModel.get_package_by_order_id(order_id)})
+        return Response({"data":OrderModel.get_all_order()})
 
 class ContainerView(APIView):
     """
     A view that can accept POST requests with JSON content.
     """
-
-    def post(self, request, format=None):
-
-        return Response({'received data': request.data})
+    def patch(self,request:Request):
+        data= request.data
+    #   "name"
+    #   "route"
+    #   "desciption"
+    #   "start_time"
+    #   "container"
+    #   "category"
+        return OrderModel.create(data)
 
     def get(self, request, format=None):
-        data = [{"id": "023", "name": "Container so 2",
-                 "start_time": "10/20/20221 10:20:15",
-                 "last_update": "2025/10/5T8:10:33", "free": False},
-                {"id": "023", "name": "Container so 2",
-                "start_time": "10/20/20221 10:20:15",
-                 "last_update": "2025/10/5T8:10:33", "free": False},
-                {"id": "023", "name": "Container so 2",
-                "start_time": "10/20/20221 10:20:15",
-                 "last_update": "2025/10/5T8:10:33", "free": True},
-                {"id": "023", "name": "Container so 2",
-                "start_time": "10/20/20221 10:20:15",
-                 "last_update": "2025/10/5T8:10:33", "free": True}]
-        return Response({'data': data})
+        containers = ContainerModel.get_sorted_container_list()
+        return Response({'data': containers})
 
 
 class ContainerMonitorView(APIView):
@@ -50,11 +43,6 @@ class ContainerMonitorView(APIView):
     def post(self, request: Request, format=None):
         id = request.data.get("container_id")
         package = PackageModel.create(container_id=id, data=request.data)
-
         return Response({"data": package})
 
-    def get(self, request: Request,  format=None):
-        id = request.query_params.get("container_id")
-        if not id:
-            raise(NotFound)
-        return Response({"data": PackageModel.get_package_by_order_id(id)})
+
