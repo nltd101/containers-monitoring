712c3b961fb887db80eb78d19934ae6d643d96c7
nltd101 <53518015+nltd101@users.noreply.github.com>
2021-11-05 10:59:13 +0700

    Update container.py

--- backend/apps/models/container.py
+++ backend/apps/models/container.py
@@ -22,7 +22,29 @@ class ContainerModel(models.Model):
             return None
         return containers.first()
 
+    @classmethod
+    def get_sorted_container_list(cls):
+        containers:QuerySet[ContainerModel] = cls.objects.all().order_by("order_id")
+        busy_container =[]
+        free_container=[]
+        for container_object in containers:
+         order:OrderModel = OrderModel.get_by_id(container_object.order_id)
+         container_data=model_to_dict(container_object)
+         if (order):
+             print(order)
+             container_data.update({"start_time":order.start_time})
+             container_data.update({"last_update":order.last_update})
+             container_data.update({"free":False})
+             busy_container.append(container_data)
 
+         else:
+             container_data.update({"start_time":None})
+             container_data.update({"last_update":None})
+             container_data.update({"free":True})
+             free_container.append(container_data)
+        busy_container.sort(key=lambda x:x["start_time"])
+        busy_container.extend(free_container)
+    return busy_container
 
     def __str__(self) -> str:
         return str(self.pk) + "_" + str(self.name)
